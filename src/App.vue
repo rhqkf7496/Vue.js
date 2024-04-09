1b9877fb0c775d06a335ad8b56b9ce2b503370df
고범석 <rhqkf7496@naver.com>
2024-04-09 14:40:41 +0900

    2024-04-09

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="black-bg" v-if="모달창 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창 = false">close</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+  </div>
+
+  <div v-for="room in 원룸들" :key="room.id">
+    <img :src="room.image" class="image" />
+    <h4 @click="openModal">{{ room.title }}</h4>
+    <p>{{ room.price }}원</p>
+    <a :href="naverUrl">naver</a>
+  </div>
+</template>
+
+<script>
+import data from "./assets/oneroom.js";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      원룸들: data,
+      메뉴들: ["Home", "Shop", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      신고수: [0, 0, 0],
+      모달창: false,
+      naverUrl:"https://www.naver.com",
+    };
+  },
+  methods: {
+    increase() {
+      this.신고수++;
+    },
+    openModal() {
+      this.모달창 = true;
+    },
+  },
+
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.image {
+  width: 15%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
